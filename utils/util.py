0fa8aaeae65eb712bef84d3ab1b27e3540d3e1e0
Jerome <jer@ABs-MacBook-Air.local>
2023-05-26 17:57:07 -0500

    remove backslash from file name

--- utils/util.py
+++ utils/util.py
@@ -6,4 +6,5 @@ for subdir, dirs, files in os.walk(path):
     for file in files:
         if file.endswith(".html"):
             filename = os.path.join(subdir, file)
-            os.rename(filename, filename.replace("#", "Event "))
+            os.rename(filename, filename.replace(":", ""))
+            print(filename.replace(":", ""))
