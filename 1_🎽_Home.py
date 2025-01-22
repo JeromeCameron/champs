1abc55527bfd613f5809c3e257d9452207abc5f7
Jerome Cameron <jcameron@digicelgroup.com>
2025-01-22 18:43:01 -0500

    corrected athlete names phase one

--- 1_🎽_Home.py
+++ 1_🎽_Home.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 # ---------------------- SETTINGS -----------------------------------#
 page_title = "Boys and Girls Champs"
@@ -6,3 +8,43 @@ page_icon = "🏃🏾"
 st.set_page_config(page_title=page_title, page_icon=page_icon)
 st.title(page_title + " " + page_icon)
 "---"
+
+
+df = pd.read_csv("./working_files/track_events.csv")
+
+# Adding medals column
+conditions = [(df["position"] == "1"), (df["position"] == "2"), (df["position"] == "3")]
+medals = ["Gold", "Silver", "Bronze"]
+
+df["medals"] = np.select(conditions, medals, "na")
+df["gold"] = np.where(df["medals"] == "Gold", 1, 0)
+df["silver"] = np.where(df["medals"] == "Silver", 1, 0)
+df["bronze"] = np.where(df["medals"] == "Bronze", 1, 0)
+
+# Based on total points
+df["points"] = df["points"].astype(float)
+pointsTable = df[["school", "year", "gender", "points"]]
+pointsTable = pointsTable[
+    (pointsTable["year"] == 24) & (pointsTable["gender"] == "Boys")
+]
+
+
+pointsTable = (
+    pointsTable.pivot_table(index="school", aggfunc=pd.Series.sum, values="points")
+    .sort_values("points", ascending=False)
+    .reset_index()
+)
+
+
+# Based on total quality of medals won
+medalTable = df[["school", "year", "gender", "gold", "silver", "bronze", "points"]]
+medalTable = medalTable[(medalTable["year"] == 24) & (medalTable["gender"] == "Boys")]
+medalTable = medalTable.pivot_table(
+    index="school", aggfunc=pd.Series.sum, values=["gold", "silver", "bronze", "points"]
+)
+medalTable = medalTable.sort_values(["gold", "silver", "bronze"], ascending=False)
+medalTable = medalTable.reindex(
+    ["gold", "silver", "bronze", "points"], axis=1
+).reset_index()
+
+st.table(medalTable)
---
+++ pages/5_🔤_About.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.header("About")
+"---"
+
+st.markdown(
+    """Boys and Girls Championship in annual track and field event in Jamaica.
+    Here high schools across the country come together in march and compete in an array of events.
+    The winning schools are determind by a points system where points awareded for position finished in the finals of each event.
+
+    Questions i want to answer:
+
+    Number of schools that participate each year.
+    Who are the top athletes interms of medals over the period?
+    Who are the top athlets based on total points over the period?
+    What would the top five schools look like if medals were used to determine the winners"""
+)
