7a5a4da6b0efadd52dffd01114c1ae929666fa28
Jerome Cameron <jcameron@digicelgroup.com>
2025-02-04 14:00:52 -0500

    add results table to results page

--- pages/2_🏃🏾_Results.py
+++ pages/2_🏃🏾_Results.py
@@ -5,22 +5,29 @@ st.header("Champs Results 🏃🏾")
 "---"
 
 df = pd.read_csv("./working_files/track_events.csv")
+df["note"] = df["note"].apply(
+    lambda x: "" if pd.isna(x) else x
+)  # Replace NaN values with an empty string
+df["mark"] = df["mark"].str.rstrip("x")
+
 
 st.text("Filter Results")
 with st.container(border=True):
-    col1, col2 = st.columns(2)
+    col1, col2, col3, col4 = st.columns(4)
     with col1:
-        year = st.text_input("Year", value=2024, max_chars=4)
-        gender = st.selectbox("Gender", ("Boys", "Girls"))
-
+        year = st.text_input("Year", value=24, max_chars=2)
     with col2:
-        clas_s = st.selectbox("Class", (1, 2, 3, "Open"))
+        gender = st.selectbox("Gender", ("Boys", "Girls"))
+    with col3:
+        clas_s = st.selectbox("Class", ("1", "2", "3", "4", "Open"))
+    with col4:
+
         discipline = st.selectbox(
             "Discipline",
             (
-                "100 Meter Dash",
-                "200 Meter Dash",
-                "400 Meter Dash",
+                "100 Meter",
+                "200 Meter",
+                "400 Meter",
                 "70 Meter Hurdles",
                 "80 Meter Hurdles",
                 "100 Meter Hurdles",
@@ -45,5 +52,56 @@ with st.container(border=True):
                 "Heptathlon",
             ),
         )
-df = df[df["year"] == 24]
-st.table(df)
+
+
+results = df[
+    (df["year"] == int(year))
+    & (df["gender"] == gender)
+    & (df["clas_s"] == clas_s)
+    & (df["event"] == discipline)
+]
+
+results.drop(["event", "gender", "clas_s", "heat", "typ", "year"], axis=1, inplace=True)
+results.sort_values(by="position", inplace=True)
+wind = results["wind"].iloc[0]
+
+st.header("Final")
+result_header = f"""
+<div style="background-color: #633974; padding: 6px; padding-left: 15px;">
+    <h4 style='color: white;'>{discipline.upper()} {gender.upper()} CLASS {clas_s} |   WIND: {wind}</h4>
+</div>
+"""
+st.markdown(result_header, unsafe_allow_html=True)
+
+# st.dataframe(
+#     results,
+#     hide_index=True,
+#     column_order=("wind", "position", "name", "school", "mark", "points", "note"),
+# )
+
+# Generate table rows dynamically
+table_rows = "".join(
+    f"<tr><td style='border: none; padding: 8px; color: #5b5b5b; text-align: center;'>{row['position']}</td>"
+    f"<td style='border: none; padding: 8px; color: #5b5b5b;'>{row['school']}</td>"
+    f"<td style='border: none; padding: 8px; color: #5b5b5b;'>{row['name']}</td>"
+    f"<td style='border: none; padding: 8px; color: #5b5b5b; text-align: center;'>{row['mark']}</td>"
+    f"<td style='border: none; padding: 8px; color: #5b5b5b; text-align: center;'>{row['points']}</td>"
+    f"<td style='border: none; padding: 8px; color: #5b5b5b;'>{row['note']}</td></tr>"
+    for _, row in results.iterrows()
+)
+
+table_html = f"""
+<table style="width:100%; border: none; border-collapse: collapse;">
+  <tr style="background-color: #273746; text-align: center; color: white;">
+    <th style="padding: 8px;">POSITION</th>
+    <th style="padding: 8px; text-align: left;">SCHOOL</th>
+    <th style="padding: 8px; text-align: left;">ATHLETE</th>
+    <th style="padding: 8px;">MARK</th>
+    <th style="padding: 8px;">POINTS</th>
+    <th style="padding: 8px;">NOTE</th>
+  </tr>
+  {table_rows}
+</table>
+"""
+
+st.markdown(table_html, unsafe_allow_html=True)
